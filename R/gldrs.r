1a1b8c20e074992bf4502a4e2d6ef308bd05020b
dougkinzey <64279035+dougkinzey@users.noreply.github.com>
2021-09-23 09:57:57 -0700

    Add files via upload
    Updated code for gldrs.r and gldry.r

--- R/gldrs.r
+++ R/gldrs.r
@@ -1,21 +1,27 @@
 # autonomous underwater glider sampling simulation
 # calls gldry()
 
-gldrs <- function(NASC.yrs = c(2001:2009,2011),AMLR.area = 'SA',n.rep = 9,n.gldr = c(1),
-                      save.tables = 0,max.NASC.m = 250,depths = c(150,1000),azfp.off = c(150,150),
-                      qntl.vals = c(0.97,0.98,0.99,0.999,1),smpl.st = 0 
-                      ){
+gldrs <- function(NASC.yrs = c(2001:2009,2011),AMLR.area = 'SA',n.rep = 9,n.gldr = c(1,2,3,4,5),
+                  save.tables = 1,max.NASC.m = 250,depths = c(150,200,300,400,500,700,1000),azfp.off = 
+                  c(150,150,150,150,150,150,150),
+                  qntl.vals = c(0.97,0.98,0.99,0.999,1),smpl.st = 0 
+                  ){
 max.z.m <- max.NASC.m #??
 if(save.tables == 1){
-    dir.create('yo_sums')
-    dir.create('gldrtables')
-    dir.create('gldr_strt')
+    dir.create('tables')
+    dir.create('tables/yo_sums')
+    dir.create('tables/gldrtables')
+    dir.create('tables/gldr_strt')
+    dir.create('tables/NASC_yrs') # operating model bin densities
+    dir.create('tables/cgldrs') # glider sampled bin densities
+    dir.create('tables/smpl_paths/') # bins sampled by gliders
+    dir.create('tables/gldr_stats/') # annual log means and SDs of glider samples
     }
 ship.Sv <- vector() # ship acoustic mean integrated returns each year
 # load the databases
 #load('NASC_leg1.RData') # acoustic data to be sampled, 199772 100m long X 100 5m deep cells
 #leg1 <- read.csv('leg1.csv') # date and sampling strata for each of 199772 columns of acoustic data
-for(i.pth in 1:length(n.gldr)) 
+for(i.pth in 1:length(n.gldr)) # i.pth indexes the number of gliders ("paths") that are sampling
 for(i.depth in 1:length(depths)){
   #path.depth <- paste(path.out,gldr.pths[i.pth],gldr.depths[i.depth],sep="")
   std.yo.depth.m <- depths[i.depth] # max yo depth
@@ -40,6 +46,7 @@ for(i.depth in 1:length(depths)){
     # NASC.vals is the subset of the entire ship database
     # that will be sampled with the glider. It is used to
     # find the Process_ID and Interval values for sampling
+    #NASC.vals <- NASC.vals[is.na(NASC.vals)] <- 0 #-dhk
 
     yo.locs <- PID <- vector()  
     # find the process and interval IDs (column names) in NASC.vals
@@ -67,15 +74,15 @@ for(i.depth in 1:length(depths)){
     yo.count[iyr] <- trunc(n.yos/2)
     max.NASC.b = max.NASC.m/5 # maximum bin depth ensonified
     NASC.dat <- NASC.leg1[1:max.NASC.b,match(yo.locs,colnames(NASC.leg1))]
+    
     # NASC.dat is the field of NASC values the glider is sampling from
     ship.Sv[iyr] <- mean(apply(NASC.dat,2,sum,na.rm=TRUE))
 
     d.bins <- length(unique(out$z.bin)) # depth bins in glider path
     h.bins <- length(unique(out$d.bin)) # horizontal bins
-    smpl.pth <- array(dim=list(d.bins,
-                       h.bins))
+    smpl.pth <- array(dim=list(d.bins,h.bins))
 		                # smpl.pth is the glider sampling matrix
-                                # calculated in R_glider_yos.txt.
+                                # calculated in gldry.r.
                                 # It is the same for each replicate
                                 # but starts in different columns of NASC.dat
     k <- 0
@@ -97,6 +104,7 @@ for(i.depth in 1:length(depths)){
                               # gldr.dat is a 4 dimensional array,
                               # depth(bins) X horizontal dist X passes X replicates
       c.gldrs <- array(dim=c((shutoff.azfp.m+azfp.range.m)/bin.depth.m,ncol(gldr.dat)*n.gldr[i.pth],n.rep))
+      # c.gldrs is a 3 dimensional array of the depths X areas sampled by all gliders X replicates
       gldr.table <- array(dim=c(n.rep,length(qntl.vals)+2))
       dimnames(gldr.table) <- list(1:n.rep,c(qntl.vals,"sum","sd"))
       y.means <- array(dim=c(nrow(gldr.dat),yo.count[iyr],n.rep)) # mean gldr densities at depth
@@ -105,16 +113,18 @@ for(i.depth in 1:length(depths)){
 
       for(i.rep in 1:n.rep){
         #set.seed()
-        ifelse(smpl.st > 0, 
+        ifelse(smpl.st > 0, # smpl.st = 1 reads gldr starting location from file
                      {
                       gldr.strt[iyr,] <-  unlist(as.numeric(read.table(paste(strt.path,
-                                     'gldr_strt_',NASC.yrs[iyr],"_",depths[i.depth],"m.txt",sep=""),header = TRUE)))
+                                     'gldr_strt_',NASC.yrs[iyr],"_",depths[i.depth],"m.txt",sep=""),
+                                      header = TRUE)))
                      },
-                      gldr.strt <- sample(1:(ncol(NASC.dat)/2),n.gldr,replace=TRUE)
+                      gldr.strt <- sample(1:(ncol(NASC.dat)/2),n.gldr[i.pth],replace=TRUE)
                      )
         # starting position is randomized for each n.gldr for the replicate in line above
         ifelse(smpl.st > 0,gldr.strt.table[i.pth,i.rep] <- gldr.strt[iyr,i.rep],
-              gldr.strt.table[i.pth,i.rep] <- gldr.strt)
+        ifelse(n.gldr == 1,gldr.strt.table[,i.rep] <- gldr.strt,
+               gldr.strt.table[i.pth,i.rep] <- gldr.strt[i.pth]))
         
         for(i.gldr in 1:n.gldr[i.pth]){
      #   if glider is not going below 500 m (NASC data limit):
@@ -134,7 +144,7 @@ for(i.depth in 1:length(depths)){
       dim(tmp.dat) <- c(nrow(gldr.dat),ncol(gldr.dat)*n.gldr[i.pth])
       c.gldrs[,,i.rep] <- tmp.dat # c.gldrs is the combined glider samples for the replicate
       smpl.pths.multi <- smpl.pths # create sampling mask for multiple gliders
-      for(i in 1:n.gldr)
+      for(i in 1:length(n.gldr))
         if(i > 1)
           smpl.pths.multi <- cbind(smpl.pths.multi,smpl.pths)
 
@@ -153,30 +163,43 @@ for(i.depth in 1:length(depths)){
       # gldr.table contains the quantiles and mean and sd of yo sums
       # for the combined gliders in each replicate      
       #####################
-    } # end of i.rep loop
+      write.table(smpl.pths.multi,paste('tables/smpl_paths/',n.gldr[i.pth],"_",
+                                 AMLR.area,'_',smpl.yr,"_",depths[i.depth],"_spths.txt",sep="")) 
+      } # end of i.rep loop
 
-    # Calculate quantiles for glider and ship NASC values for the year
+    write.table(round(c.gldrs,5),paste('tables/cgldrs/',n.gldr[i.pth],"_",
+                                 AMLR.area,'_',smpl.yr,"_",depths[i.depth],"_cgldrs.txt",sep="")) 
+
+     # Calculate quantiles for glider and ship NASC values for the year
     NASC.table <- c(quantile(NASC.dat,
       probs = qntl.vals,na.rm=TRUE),mean(apply(NASC.dat,2,sum,na.rm=TRUE)),sd(apply(NASC.dat,2,sum,na.rm=TRUE)))
     ship.table[iyr,] <- c(NASC.yrs[iyr],NASC.table)
 
     if(save.tables == 1){
       write.table(round(gldr.table,digits=1),
-        file=paste('gldrtables/',smpl.yr,'_',depths[i.depth],"m_gldrtable.txt",sep=""))
-      write.table(gldr.strt.table,paste("gldr_strt/gldr_strt_",NASC.yrs[iyr],"_",depths[i.depth],"m.txt",sep=""))
-      write.table(round(yo.sums,5),paste("yo_sums/",smpl.yr,"_",depths[i.depth],"_yo_sums.txt",sep=""))
+        file=paste('tables/gldrtables/',n.gldr[i.pth],"_",
+         AMLR.area,'_',smpl.yr,'_',depths[i.depth],"m_gldrtable.txt",sep=""))
+      write.table(gldr.strt.table,paste("tables/gldr_strt/",
+         n.gldr[i.pth],'_', AMLR.area,"_gldr_strt_",NASC.yrs[iyr],"_",depths[i.depth],"m.txt",sep=""))
+      write.table(round(yo.sums,5),paste("tables/yo_sums/",
+         n.gldr[i.pth],'_', AMLR.area,"_",smpl.yr,"_",depths[i.depth],"_yo_sums.txt",sep=""))
+      write.csv(round(t(NASC.dat),4),
+         file=paste('tables/NASC_yrs/',n.gldr[i.pth],"_",AMLR.area,"_",smpl.yr,"_NASC_dat.csv",sep=""))
       }
       #####################
     } # end iyr loop
   ship.table[,2:8] <- round(ship.table[,2:8],digits=1)
 
   if(save.tables == 1){
-    write(ship.Sv,"ship_Sv.txt",ncolumns=10)
+    write(ship.Sv,"tables/ship_Sv.txt",ncolumns=10)
     write.table(ship.table,row.names=FALSE,
-        file=paste("shiptable.txt",sep=""))
-    write.table(round(gldr.sums,5),paste(depths[i.depth],"_gldr_sums.txt",sep=""))
-    write.table(round(gldr.sds,5),paste(depths[i.depth],"_gldr_sd.txt",sep=""))
-    write(yo.count*n.gldr[i.pth],paste("yo_count.txt",sep=""),ncolumns=10)
+        file=paste("tables/shiptable.txt",sep=""))
+    write.table(round(gldr.sums,5),paste("tables/gldr_stats/",
+        n.gldr,'_', AMLR.area,"_",depths[i.depth],"_gldr_sums.txt",sep=""))
+    write.table(round(gldr.sds,5),paste('tables/gldr_stats/',
+        n.gldr,'_', AMLR.area,"_",depths[i.depth],"_gldr_sd.txt",sep=""))
+    write(yo.count*n.gldr[i.pth],paste("tables/",
+        n.gldr,'_', AMLR.area,'_',depths[i.depth],"_yo_count.txt",sep=""),ncolumns=10)
   } # end save.tables
- } # end of i.depth loop
+ } # end of i.depth, n.gldr loops
 } # end function gldrs()
